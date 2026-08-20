6a6294bd72c59d220f6ef6526d540ac8a720aad5
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-08-20 22:37:03 +1000

    Splitting the dataset into 90/10 and using a random state 1

--- load.py
+++ load.py
@@ -54,3 +54,8 @@ y = dataset['Diagnosis']
 #Quick check
 print(x.shape) # Output is (297, 13)
 print(y.shape) # Output is (297, )
+
+#Splitting the dataset and normalisaing the data
+#(Using 10% of dataset for testing with a random state of 1 )
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state=1)
