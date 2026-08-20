64209bce547224713f10e58ea320fbd9cff6e87f
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-08-20 22:10:26 +1000

    Changing datatype from float to int where needed

--- load.py
+++ load.py
@@ -1,7 +1,22 @@
 #Loading the dataset
 import pandas as pd
-def load_dataset():
-    return pd.read_csv('heart_disease.csv')
-dataset = load_dataset()
+dataset = pd.read_csv('heart_disease.csv')
 print(f'The length of the dataset is {len(dataset)}')
 print(dataset.head())
+
+#Checking if there is any missing values
+print(dataset.isna().sum())
+
+#Dropping the columns with missing values
+dataset = dataset.dropna()
+print(f'The length of the dataset after dropping the missing values is {len(dataset)}')
+
+#Checking the variable data types
+print(dataset.dtypes)
+
+# Number of vessels and Thal is represented as float but it should be int
+cols = ['Number of major vessels', 'thal']
+dataset[cols] = dataset[cols].astype(int)
+
+#checking the datatype after changing from float to int
+print(dataset.dtypes)
